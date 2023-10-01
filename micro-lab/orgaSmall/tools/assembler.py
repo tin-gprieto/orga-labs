c234e98b056138724c6806bc55051e04937037ca
tin-gprieto <mgonzalezp@fi.uba.ar>
2023-10-01 19:53:33 -0300

    Avance checkpoint 4

--- micro-lab/orgaSmall/tools/assembler.py
+++ micro-lab/orgaSmall/tools/assembler.py
@@ -5,7 +5,7 @@ from common import *
 type_RR = ["ADD", "ADC", "SUB", "AND", "OR", "XOR", "CMP", "MOV"]
 type_RM = ["STR", "LOAD"]
 type_M = ["JMP", "JC", "JZ", "JN"]
-type_R = ["INC", "DEC"]
+type_R = ["INC", "DEC", "SIG"]
 type_RS = ["SHR", "SHL"]
 type_RI = ["SET"]
 def_DB = ["DB"]
@@ -19,6 +19,7 @@ opcodes = {
     "XOR": 6,
     "CMP": 7,
     "MOV": 8,
+    "SIG": 9,
     "STR": 16,
     "LOAD": 17,
     "STRr": 18,
