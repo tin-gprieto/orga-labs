c6f95658cb5e88ea6e1e6e731f5205d67b37d151
tin-gprieto <mgonzalezp@fi.uba.ar>
2023-10-03 18:22:41 -0300

    feat: add MIX instruction

--- micro-lab/orgaSmall/tools/assembler.py
+++ micro-lab/orgaSmall/tools/assembler.py
@@ -2,7 +2,7 @@
 from common import *
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-type_RR = ["ADD", "ADC", "SUB", "AND", "OR", "XOR", "CMP", "MOV"]
+type_RR = ["ADD", "ADC", "SUB", "AND", "OR", "XOR", "CMP", "MOV", "MIX"]
 type_RM = ["STR", "LOAD"]
 type_M = ["JMP", "JC", "JZ", "JN"]
 type_R = ["INC", "DEC", "SIG"]
@@ -20,6 +20,7 @@ opcodes = {
     "CMP": 7,
     "MOV": 8,
     "SIG": 9,
+    "MIX": 10,
     "STR": 16,
     "LOAD": 17,
     "STRr": 18,
